8ae6297e627e5634fbfdf632f8cc7792e0725ce6
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-05-21 10:21:13 +0700

    Display multiple Todo items

--- GCD0804TodoManagement/Controllers/TodoesController.cs
+++ GCD0804TodoManagement/Controllers/TodoesController.cs
@@ -1,22 +1,38 @@
 ﻿using GCD0804TodoManagement.Models;
 using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace GCD0804TodoManagement.Controllers
 {
 	public class TodoesController : Controller
 	{
-		private Todo _todo = new Todo()
+		private List<Todo> _todoes = new List<Todo>();
+
+		public TodoesController()
 		{
-			Id = 1,
-			Category = "Family",
-			Description = "Buy Milk",
-			DueDate = DateTime.Now
-		};
+			_todoes.Add(new Todo()
+			{
+				Id = 1,
+				Category = "Family",
+				Description = "Buy Milk",
+				DueDate = DateTime.Now
+			});
+
+			_todoes.Add(new Todo()
+			{
+				Id = 2,
+				Category = "Professional",
+				Description = "Report the Progress of the Project",
+				DueDate = DateTime.Now
+			});
+
+		}
+
 		// GET: Todoes
 		public ActionResult Index()
 		{
-			return View(_todo);
+			return View(_todoes);
 		}
 	}
 }
