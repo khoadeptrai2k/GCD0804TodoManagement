bd6d2b3636c1d966e4fea8d2fdd4abe2c2f04cc7
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-05-21 10:01:51 +0700

    Display a single Todo item

---
+++ GCD0804TodoManagement/Controllers/TodoesController.cs
@@ -0,0 +1,22 @@
+﻿using GCD0804TodoManagement.Models;
+using System;
+using System.Web.Mvc;
+
+namespace GCD0804TodoManagement.Controllers
+{
+	public class TodoesController : Controller
+	{
+		private Todo _todo = new Todo()
+		{
+			Id = 1,
+			Category = "Family",
+			Description = "Buy Milk",
+			DueDate = DateTime.Now
+		};
+		// GET: Todoes
+		public ActionResult Index()
+		{
+			return View(_todo);
+		}
+	}
+}
