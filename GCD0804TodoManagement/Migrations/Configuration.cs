60c553df6f0f73f4a04cf86988e7993c2883e688
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-05-25 07:53:56 +0700

    First Migration: CreateTodoesTable

---
+++ GCD0804TodoManagement/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace GCD0804TodoManagement.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GCD0804TodoManagement.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(GCD0804TodoManagement.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
