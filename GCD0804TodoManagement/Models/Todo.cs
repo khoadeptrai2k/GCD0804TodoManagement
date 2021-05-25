437355f3e4f0ef4247433d9de18aeaa9d3ad0823
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-05-25 09:34:44 +0700

    Add Required to Todo.cs and Migrate

--- GCD0804TodoManagement/Models/Todo.cs
+++ GCD0804TodoManagement/Models/Todo.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GCD0804TodoManagement.Models
 {
 	public class Todo
 	{
+		[Required]
 		public int Id { get; set; }
+		[Required]
 		public string Description { get; set; }
+		[Required]
+		[Display(Name = "Due Date of the Todo")]
 		public DateTime DueDate { get; set; }
+		[Required]
 		public string Category { get; set; }
 	}
 }
