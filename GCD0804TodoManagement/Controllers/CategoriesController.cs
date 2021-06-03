b641588b53e6c3a73b105b1c1e8e0405a2796e63
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-06-03 15:19:47 +0700

    Add CategoriesController skeleton

---
+++ GCD0804TodoManagement/Controllers/CategoriesController.cs
@@ -0,0 +1,35 @@
+﻿using GCD0804TodoManagement.Models;
+using System.Web.Mvc;
+
+namespace GCD0804TodoManagement.Controllers
+{
+  [Authorize]
+  public class CategoriesController : Controller
+  {
+    private ApplicationDbContext _context;
+    public CategoriesController()
+    {
+      _context = new ApplicationDbContext();
+    }
+    // GET: Categories
+    [Authorize(Roles = "user, manager")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "manager")]
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "manager")]
+    public ActionResult Create(Category model)
+    {
+      return RedirectToAction("Index");
+    }
+  }
+}
