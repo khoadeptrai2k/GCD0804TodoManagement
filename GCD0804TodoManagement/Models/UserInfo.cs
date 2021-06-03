6db70126b065c2262bd81e19bfb9b85fc086e12b
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-06-03 13:39:10 +0700

    Tab 4 -> Tab 2

--- GCD0804TodoManagement/Models/UserInfo.cs
+++ GCD0804TodoManagement/Models/UserInfo.cs
@@ -3,16 +3,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GCD0804TodoManagement.Models
 {
-	public class UserInfo
-	{
-		[Key]
-		[ForeignKey("User")]
-		public string UserId { get; set; }
-		public ApplicationUser User { get; set; }
-		[Required]
-		public string FullName { get; set; }
-		[Required]
-		[Range(1, 150, ErrorMessage = "Please enter Age value bigger than 0 and less than 150")]
-		public int Age { get; set; }
-	}
+  public class UserInfo
+  {
+    [Key]
+    [ForeignKey("User")]
+    public string UserId { get; set; }
+    public ApplicationUser User { get; set; }
+    [Required]
+    public string FullName { get; set; }
+    [Required]
+    [Range(1, 150, ErrorMessage = "Please enter Age value bigger than 0 and less than 150")]
+    public int Age { get; set; }
+  }
 }
