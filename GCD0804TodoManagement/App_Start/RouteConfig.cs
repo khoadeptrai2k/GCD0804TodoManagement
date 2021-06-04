e6621d151d19b2b2e92f0806b82c5f1875209883
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-06-04 10:24:28 +0700

    Implement Remove User from Team

--- GCD0804TodoManagement/App_Start/RouteConfig.cs
+++ GCD0804TodoManagement/App_Start/RouteConfig.cs
@@ -1,23 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace GCD0804TodoManagement
 {
-	public class RouteConfig
-	{
-		public static void RegisterRoutes(RouteCollection routes)
-		{
-			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+  public class RouteConfig
+  {
+    public static void RegisterRoutes(RouteCollection routes)
+    {
+      routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-			routes.MapRoute(
-					name: "Default",
-					url: "{controller}/{action}/{id}",
-					defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-			);
-		}
-	}
+      routes.MapRoute(
+          name: "Default",
+          url: "{controller}/{action}/{id}",
+          defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+      );
+
+      routes.MapRoute(
+        name: "RemoveUser",
+        url: "{controller}/{action}/{id}/{userid}",
+        new { controller = "Teams", action = "RemoveUser", id = "", userId = "" }
+      );
+    }
+  }
 }
--- GCD0804TodoManagement/Controllers/TeamsController.cs
+++ GCD0804TodoManagement/Controllers/TeamsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 
 namespace GCD0804TodoManagement.Controllers
@@ -34,6 +35,8 @@ namespace GCD0804TodoManagement.Controllers
         .Where(t => t.TeamId == id)
         .Select(t => t.User)
         .ToList();
+
+      ViewBag.TeamId = id;
 
       return View(users);
     }
@@ -81,6 +84,20 @@ namespace GCD0804TodoManagement.Controllers
       _context.SaveChanges();
 
       return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public ActionResult RemoveUser(int id, string userId)
+    {
+      var teamUser = _context.TeamsUsers
+        .SingleOrDefault(t => t.TeamId == id && t.UserId == userId);
+
+      if (teamUser == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+      _context.TeamsUsers.Remove(teamUser);
+      _context.SaveChanges();
+
+      return RedirectToAction("Details", new { id = id });
     }
   }
 }
