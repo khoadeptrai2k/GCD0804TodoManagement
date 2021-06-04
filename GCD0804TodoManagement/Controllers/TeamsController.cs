e54f0ca41488181789d6c1075f6af781e87854e6
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-06-04 07:56:26 +0700

    Implement /Teams/Index action

---
+++ GCD0804TodoManagement/Controllers/TeamsController.cs
@@ -0,0 +1,23 @@
+﻿using GCD0804TodoManagement.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GCD0804TodoManagement.Controllers
+{
+  [Authorize(Roles = "manager")]
+  public class TeamsController : Controller
+  {
+    private ApplicationDbContext _context;
+    public TeamsController()
+    {
+      _context = new ApplicationDbContext();
+    }
+    // GET: Teams
+    [HttpGet]
+    public ActionResult Index()
+    {
+      var teams = _context.Teams.ToList();
+      return View(teams);
+    }
+  }
+}
