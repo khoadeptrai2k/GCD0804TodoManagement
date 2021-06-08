ba90f16197464c2da258350640d419654d67b60f
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-06-08 10:47:21 +0700

    Build Teams API

---
+++ GCD0804TodoManagement/Controllers/Api/TeamsController.cs
@@ -0,0 +1,70 @@
+﻿using GCD0804TodoManagement.Models;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace GCD0804TodoManagement.Controllers.Api
+{
+  public class TeamsController : ApiController
+  {
+    private ApplicationDbContext _context;
+    public TeamsController()
+    {
+      _context = new ApplicationDbContext();
+    }
+    [HttpGet]
+    public IHttpActionResult GetAll()
+    {
+      var teams = _context.Teams.ToList();
+      return Ok(teams);
+    }
+    [HttpGet]
+    public IHttpActionResult GetById(int id)
+    {
+      var team = _context.Teams.SingleOrDefault(t => t.Id == id);
+      if (team == null) return NotFound();
+
+      return Ok(team);
+    }
+
+    [HttpPost]
+    public IHttpActionResult Create([FromBody] Team model)
+    {
+      var newTeam = new Team
+      {
+        Name = model.Name
+      };
+
+      _context.Teams.Add(newTeam);
+      _context.SaveChanges();
+
+      return StatusCode(HttpStatusCode.Created);
+    }
+
+    [HttpDelete]
+    public IHttpActionResult Delete(int id)
+    {
+      var team = _context.Teams.SingleOrDefault(t => t.Id == id);
+
+      if (team == null) return NotFound();
+
+      _context.Teams.Remove(team);
+      _context.SaveChanges();
+
+      return Ok("Deleted ...");
+    }
+
+    [HttpPut]
+    public IHttpActionResult Edit(int id, [FromBody] Team model)
+    {
+      var team = _context.Teams.SingleOrDefault(t => t.Id == id);
+
+      if (team == null) return NotFound();
+
+      team.Name = model.Name;
+      _context.SaveChanges();
+
+      return Ok("Updated ...");
+    }
+  }
+}
--- GCD0804TodoManagement/Global.asax.cs
+++ GCD0804TodoManagement/Global.asax.cs
@@ -1,21 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace GCD0804TodoManagement
 {
-    public class MvcApplication : System.Web.HttpApplication
+  public class MvcApplication : System.Web.HttpApplication
+  {
+    protected void Application_Start()
     {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
+      GlobalConfiguration.Configure(WebApiConfig.Register);
+      AreaRegistration.RegisterAllAreas();
+      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
     }
+  }
 }
